78959b57a4dd86614ee3c1444dcafd577791bd23
Dawid <dawid130892@interia.pl>
2023-07-30 12:12:53 +0200

    Fixed problem with computer imgs replacing

--- script.js
+++ script.js
@@ -7,15 +7,19 @@ const computerPaper = document.querySelector(".computerpaper");
 const computerRock = document.querySelector(".computerrock");
 
 const reset = document.querySelector(".reset");
+const choice = document.querySelector(".choice");
 // Assigned user choice by click on img
 reset.addEventListener("click", resetHiding);
 function resetHiding() {
 	const images = document.querySelectorAll("img");
 	images.forEach((img) => {
 		img.style.display = "inline-block";
-        scissors.setAttribute("src", "img/scissors1.jpg");
-        paper.setAttribute("src", "img/paper1.jpg");
-        rock.setAttribute("src", "img/rock1.jpg");
+		scissors.setAttribute("src", "img/scissors1.jpg");
+		paper.setAttribute("src", "img/paper1.jpg");
+		rock.setAttribute("src", "img/rock1.jpg");
+		computerScissors.setAttribute("src", "img/scissors1.jpg");
+		computerPaper.setAttribute("src", "img/paper1.jpg");
+		computerRock.setAttribute("src", "img/rock1.jpg");
 	});
 }
 let userChoice;
@@ -39,7 +43,10 @@ function getRandomChoice() {
 	const randomIndex = Math.floor(Math.random() * choiceArray.length);
 	return choiceArray[randomIndex];
 }
-
+function replaceButton() {
+	reset.style.display = "block";
+	choice.style.display = "none";
+}
 function getResult() {
 	let computerWin = "wygrywa komputer!";
 	let draw = "mamy remis!";
@@ -90,16 +97,21 @@ function getResult() {
 		scissors.setAttribute("src", "img/scissors2.jpg");
 	} else if (userChoice === "nożyce" && computerChoice === "papier") {
 		result = playerWin;
+		computerPaper.setAttribute("src", "img/paper2.jpg");
 	} else if (userChoice === "kamień" && computerChoice === "nożyce") {
 		result = playerWin;
+		computerScissors.setAttribute("src", "img/scissors2.jpg");
 	} else if (userChoice === "kamień" && computerChoice === "papier") {
 		result = computerWin;
+		rock.setAttribute("src", "img/rock2.jpg");
 	} else if (userChoice === "kamień" && computerChoice === "kamień") {
 		result = draw;
 	} else if (userChoice === "papier" && computerChoice === "nożyce") {
 		result = computerWin;
+		paper.setAttribute("src", "img/paper2.jpg");
 	} else if (userChoice === "papier" && computerChoice === "kamień") {
 		result = playerWin;
+		computerRock.setAttribute("src", "img/rock2.jpg");
 	} else if (userChoice === "papier" && computerChoice === "papier") {
 		result = draw;
 	}
@@ -109,6 +121,7 @@ function getResult() {
 	compHiderp();
 	compHidesp();
 	compHiders();
+	replaceButton();
 	console.log(
 		`Komputer wybral ${computerChoice} a gracz ${userChoice}. A więc ${result} `
 	);
